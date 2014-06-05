d5ff4831655f492a76f9101b8ef083dd3960ad61
yangxiaohao <admin@yangxiaohao.com>
2014-06-05 16:55:40 +0800

    initial

---
+++ Hash/Hash/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
